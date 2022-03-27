9561ad7ea05485bad4b2aae60d78a9acbc8e19c6
HabibArouaXelero <habib.a@xelero.io>
2022-03-27 18:30:06 +0100

    add auth form

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get
+(
+    '/login1', 
+    function () 
+    {
+        return view('auth.login1');
+    }
+);
