fc9e94621717cc3fa28d1edbd3ab3abf8581de17
HabibArouaXelero <habib.a@xelero.io>
2022-04-07 17:34:16 +0100

    Update register1.blade.php

--- resources/views/auth/register1.blade.php
+++ resources/views/auth/register1.blade.php
@@ -1,82 +1,81 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-  @include('auth.stylesheet')
-</head>
-
-<body>
-    <div class="container-xxl position-relative bg-white d-flex p-0">
-        <!-- Spinner Start -->
-        <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
-            <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
-                <span class="sr-only">Loading...</span>
-            </div>
-        </div>
-        <!-- Spinner End -->
-        <!-- Sign Up Start -->
-        <div class="container-fluid">
-            <div class="row h-100 align-items-center justify-content-center" style="min-height: 100vh;">
-                <div class="col-12 col-sm-8 col-md-6 col-lg-5 col-xl-4">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-                        <div class="bg-light rounded p-4 p-sm-5 my-4 mx-3">
-                            <div class="d-flex align-items-center justify-content-between mb-3">
-                                <a href="index.html" class="">
-                                    <h3 class="text-primary"><i class="fa fa-hashtag me-2"></i>DASHMIN</h3>
-                                </a>
-                                <h3>Sign Up</h3>
-                            </div>
-                            <div class="form-floating mb-3">
-                                <input type="text" class="form-control" name="name" id="floatingText" placeholder="jhondoe" />
-                                <label for="floatingText">Username</label>
-                            </div>
-                            <div class="form-floating mb-3">
-                                <input type="email" class="form-control" name="email" id="floatingInput" placeholder="name@example.com" />
-                                <label for="floatingInput">Email address</label>
-                            </div>
-                            <div class="form-floating mb-4">
-                                <input type="password" name="password" class="form-control" id="floatingPassword" placeholder="Password" autocomplete="new-password">
-                                <label for="floatingPassword">Password</label>
-                            </div>
-                            <div class="form-floating mb-4">
-                                <input type="password" name="password_confirmation" class="form-control" id="floatingPassword" placeholder="Password" autocomplete="new-password" >
-                                <label for="floatingPassword">Password</label>
-                            </div>
-                            <div class="d-flex align-items-center justify-content-between mb-4">
-                                <div class="form-check">
-                                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                                </div>
-                                <a href="">Forgot Password</a>
-                            </div>
-                            <button type="submit" class="btn btn-primary py-3 w-100 mb-4">Sign Up</button>
-                            <p class="text-center mb-0">Already have an Account? <a href="">Sign In</a></p>
-                        </div>
-                    </form>
+    <head>
+        @include('auth.stylesheet')
+    </head>
+    <body>
+        <div class="container-xxl position-relative bg-white d-flex p-0">
+            <!-- Spinner Start -->
+            <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+                <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
+                    <span class="sr-only">Loading...</span>
                 </div>
             </div>
+            <!-- Spinner End -->
+            <!-- Sign Up Start -->
+            <div class="container-fluid">
+                <div class="row h-100 align-items-center justify-content-center" style="min-height: 100vh;">
+                    <div class="col-12 col-sm-8 col-md-6 col-lg-5 col-xl-4">
+                        <form method="POST" action="{{ route('register') }}">
+                            @csrf
+                            <div class="bg-light rounded p-4 p-sm-5 my-4 mx-3">
+                                <div class="d-flex align-items-center justify-content-between mb-3">
+                                    <a href="index.html" class="">
+                                        <h3 class="text-primary"><i class="fa fa-hashtag me-2"></i>DASHMIN</h3>
+                                    </a>
+                                    <h3>Sign Up</h3>
+                                </div>
+                                <div class="form-floating mb-3">
+                                    <input type="text" class="form-control" name="name" id="floatingText" placeholder="jhondoe" />
+                                    <label for="floatingText">Username</label>
+                                </div>
+                                <div class="form-floating mb-3">
+                                    <input type="email" class="form-control" name="email" id="floatingInput" placeholder="name@example.com" />
+                                    <label for="floatingInput">Email address</label>
+                                </div>
+                                <div class="form-floating mb-4">
+                                    <input type="password" name="password" class="form-control" id="floatingPassword" placeholder="Password" autocomplete="new-password">
+                                    <label for="floatingPassword">Password</label>
+                                </div>
+                                <div class="form-floating mb-4">
+                                    <input type="password" name="password_confirmation" class="form-control" id="floatingPassword" placeholder="Password" autocomplete="new-password" >
+                                    <label for="floatingPassword">Password</label>
+                                </div>
+                                <div class="d-flex align-items-center justify-content-between mb-4">
+                                    <div class="form-check">
+                                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                                    </div>
+                                    <a href="">Forgot Password</a>
+                                </div>
+                                <button type="submit" class="btn btn-primary py-3 w-100 mb-4" id="btClick">Sign Up</button>
+                                <p class="text-center mb-0">Already have an Account? <a href="">Sign In</a></p>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            @error('name')
+                <script>
+                    alert("{{ $message }}");
+                </script>
+            @enderror
+
+            @error('email')
+                <script>
+                    alert("{{ $message }}");
+                </script>
+            @enderror
+
+            @error('password')
+                <script>
+                    alert("{{ $message }}");
+                </script>
+            @enderror
+            <!-- Sign Up End -->
         </div>
-        @error('name')
-            <script>
-                alert("{{ $message }}");
-            </script>
-        @enderror
-
-        @error('email')
-            <script>
-                alert("{{ $message }}");
-            </script>
-        @enderror
-
-        @error('password')
-            <script>
-                alert("{{ $message }}");
-            </script>
-        @enderror
-        <!-- Sign Up End -->
-    </div>
-    @include('auth.js')
-</body>
-
+        @include('auth.js')
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <script src={{ asset('js/admin/register.js') }}></script>
+    </body>
 </html>
